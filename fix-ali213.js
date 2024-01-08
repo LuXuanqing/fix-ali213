fb8c64c7a9c38728bcbdfc6ba3ba3eb31a6f8fb4
陆玄青 <lu.xuanqing@outlook.com>
2024-01-08 23:52:09 +0800

    add @homepage

--- fix-ali213.js
+++ fix-ali213.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         修复游侠图片不展示问题
 // @namespace    http://tampermonkey.net/
-// @version      0.2.1
+// @version      0.2.2
 // @description  fix img display
 // @author       Xuanqing Lu
 // @match        https://down.ali213.net/pcgame/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ali213.net
 // @grant        none
 // @license      MIT
+// @homepage     https://github.com/LuXuanqing/fix-ali213/tree/main
 // @supportURL   https://github.com/LuXuanqing/fix-ali213/tree/main
 // @updateURL    https://update.greasyfork.org/scripts/484249/%E4%BF%AE%E5%A4%8D%E6%B8%B8%E4%BE%A0%E5%9B%BE%E7%89%87%E4%B8%8D%E5%B1%95%E7%A4%BA%E9%97%AE%E9%A2%98.meta.js
 // @downloadURL  https://update.greasyfork.org/scripts/484249/%E4%BF%AE%E5%A4%8D%E6%B8%B8%E4%BE%A0%E5%9B%BE%E7%89%87%E4%B8%8D%E5%B1%95%E7%A4%BA%E9%97%AE%E9%A2%98.user.js
