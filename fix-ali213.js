ed2ef7254d9b4889cdfad250d841733229113859
陆玄青 <lu.xuanqing@outlook.com>
2024-01-08 23:44:58 +0800

    Update fix-ali213.js

--- fix-ali213.js
+++ fix-ali213.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         修复游侠图片不展示问题
 // @namespace    http://tampermonkey.net/
-// @version      0.1.2
+// @version      0.2.0
 // @description  fix img display
 // @author       Xuanqing Lu
 // @match        https://down.ali213.net/pcgame/*
@@ -15,11 +15,18 @@
 
 (function () {
     'use strict';
+    // fix pcgame list img src 
     const nodes = document.querySelectorAll('.content-a>img')
     for (const node of nodes) {
         const url = node.getAttribute('data-original')
         if (url) {
             node.src = url
         }
+    }
+
+    // scroll to game list smoothly
+    const list = document.querySelector('.famous-ul-container')
+    if (list) {
+        list.scrollIntoView({ behavior: 'smooth' })
     }
 })();
